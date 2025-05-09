7cb2e584ce64cb558860545322ab89a3710b9496
Ivan Kodak <ivan.kodak@jcash.co>
2025-05-09 13:23:34 +0100

    feat: implement code snippet for "invalid value for value.authorizationList" error

---
+++ src/eip-7702-issue.ts
@@ -0,0 +1,25 @@
+import {ethers, FetchRequest, Network} from "ethers";
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+async function main() {
+    try {
+        const chainId = 97;
+        const fetchRequest = new FetchRequest('https://bsc-rpc.publicnode.com');
+
+        // NOTE: by default batching is enable, "batchMaxCount: 1" disables requests batching
+        const provider = new ethers.JsonRpcProvider(fetchRequest, chainId, {
+            staticNetwork: Network.from(chainId),
+            batchMaxCount: 1,
+        });
+
+        await provider.getBlock(49364730, true)
+    } catch (e){
+        // TODO: error "server response 401 Unauthorized" from waitForTransaction doesn't come into this catch
+        console.error('TEST', e);
+    }
+}
+
+main()
